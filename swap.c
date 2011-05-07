54ac50ebd68575d836082b2d2777b384e2296ac5
Ahmet Alp Balkan <ahmet@ahmetalpbalkan.com>
2011-05-07 17:17:16 +0300

    license&author comments, code comment fixes

--- swap.c
+++ swap.c
@@ -1,3 +1,19 @@
+/* swap - exchange names of two files or folders
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Authors : ahmet alp balkan <ahmetalpbalkan at gmail.com>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -26,7 +42,7 @@ int main(int argc,char** argv){
 	
 	// second rename
 	if (rename(fn2, fn1) == -1){
-		// recover previous rename
+		// revert previous rename
 		rename(fn1_tmp, fn1); // do not handle err
 		
 		perror(REN_ERR);
@@ -35,7 +51,7 @@ int main(int argc,char** argv){
 	
 	// third rename
 	if (rename(fn1_tmp, fn2) == -1){
-		// recover previous renames
+		// revert previous renames
 		rename(fn1, fn2); // do not handle error
 		rename(fn1_tmp, fn1); // do not handle error
 		
